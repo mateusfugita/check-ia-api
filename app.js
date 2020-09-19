6f98887f199c6046f65d8a9ea8fb4afe9f579f5e
Mateus Fugita <mateushiwao@gmail.com>
2020-09-19 18:04:47 -0300

    add cors

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const app = express();
-require('dotenv').config();
+const cors = require('cors');
 const routes = require('./routes');
+require('dotenv').config();
 
+const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
